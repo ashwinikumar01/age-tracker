6825d3ff5bbff9b83818281ec5942d62a60560df
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2021-05-07 22:24:32 +0530

    feat: added reusable card

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"></input>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
