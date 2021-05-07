617cf361dcf63869b90d52f5a4a1ab44dcf73294
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2021-05-07 22:45:20 +0530

    feat: username & age added

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+  };
+
+  const userNameHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input id="username" type="text" onChange={userNameHandler}></input>
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"></input>
+        <input id="age" type="number" onChange={ageHandler}></input>
         <Button type="submit" children="Add User" />
       </form>
     </Card>
@@ -22,3 +34,4 @@ const AddUser = (props) => {
 };
 
 export default AddUser;
+// enteredUsername will store current snapshot as well as latest snapshot
